820b91bb71b23a677804822bf01d3d78858af278
萌汉曜 <857951637@qq.com>
2019-12-27 17:44:15 +0800

    P33 , feat: edit question

--- src/main/java/com/example/community/controller/PublishController.java
+++ src/main/java/com/example/community/controller/PublishController.java
@@ -1,13 +1,16 @@
 package com.example.community.controller;
 
+import com.example.community.dto.QuestionDTO;
 import com.example.community.mapper.QuestionMapper;
 import com.example.community.mapper.UserMapper;
 import com.example.community.model.Question;
 import com.example.community.model.User;
+import com.example.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -17,21 +20,34 @@ import javax.servlet.http.HttpServletRequest;
 public class PublishController {
 
     @Autowired
-    private QuestionMapper questionMapper;
+     private QuestionService questionService;
 
-    @Autowired
-    private UserMapper userMapper;
-
+    //  展示列表
     @GetMapping("/publish")
     public String publish() {
         return "publish";
     }
 
+    //  展示具体内容（供修改）
+    @GetMapping("/publish/{id}")
+    public String edit(@PathVariable(name = "id") Integer id,
+                       Model model) {
+        QuestionDTO question = questionService.getById(id);
+        model.addAttribute("title", question.getTitle());
+        model.addAttribute("description", question.getDescription());
+        model.addAttribute("tag", question.getTag());
+        //  id标签做修改时使用
+        model.addAttribute("id", question.getId());
+        return "publish";
+    }
+
+    //  增
     @PostMapping("/publish")
     public String doPublish(
-            @RequestParam("title") String title,
-            @RequestParam("description") String description,
-            @RequestParam("tag") String tag,
+            @RequestParam(value = "title", required = false) String title,
+            @RequestParam(value = "description", required = false) String description,
+            @RequestParam(value = "tag", required = false) String tag,
+            @RequestParam(value = "id", required = false) Integer id,
             HttpServletRequest request,
             Model model) {
 
@@ -75,12 +91,12 @@ public class PublishController {
             return "/publish";
         }
         question.setCreator(user.getId());
-        question.setGmtCreate(System.currentTimeMillis());
-        question.setGmtModified(question.getGmtCreate());
         question.setViewCount(0);
         question.setCommentCount(0);
         question.setLikeCount(0);
-        questionMapper.create(question);
+        //  id是增或改的判断
+        question.setId(id);
+        questionService.createOrUpdate(question);
         return "redirect:/";
     }
 }
